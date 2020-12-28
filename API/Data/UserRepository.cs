d41d2c91f357a03367e47c11980d1dfeb8d24405
Matthias Deisböck <matthias.deisboeck@gmail.com>
2020-12-28 17:40:05 +0100

    End of section 8

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public UserRepository(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            this._mapper = mapper;
+        }
+
+        public async Task<MemberDto> GetMemberAsync(string username) 
+            => await _context.Users.Where(x => x.UserName.Equals(username))
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider).SingleOrDefaultAsync();
+
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync() 
+            => await _context.Users
+                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider).ToListAsync();
+
+        public async Task<bool> SaveAllAsync() => await _context.SaveChangesAsync() > 0;
+
+        public void Update(AppUser user) => _context.Entry(user).State = EntityState.Modified;
+    }
+}
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+}
